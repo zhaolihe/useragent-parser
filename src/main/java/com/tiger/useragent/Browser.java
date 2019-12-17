67b855887a3c2db5f5bfdf923cd2d9c88cc9c865
zhaolihe <dayingzhaolihe@126.com>
2019-12-17 15:14:58 +0800

    对外提供单独解析device，os，browser的方法

--- src/main/java/com/tiger/useragent/Browser.java
+++ src/main/java/com/tiger/useragent/Browser.java
@@ -6,17 +6,37 @@ package com.tiger.useragent;
  * email : dayingzhaolihe@126.com
  * date : 2017/5/5
  */
-class Browser {
-    public static final Browser DEFAULT_BROWSER = new Browser("-","-",null,null);
-    public final String brand; //品牌
-    public final String family;
-    public final String major;
-    public final String minor;
+public class Browser {
+    static final Browser DEFAULT_BROWSER = new Browser("-", "-", null, null);
+    final String brand; //品牌
+    final String family;
+    final String major;
+    final String minor;
 
-    Browser(String brand,String family,String major,String minor){
+    Browser(String brand, String family, String major, String minor) {
         this.brand = brand;
         this.family = family;
         this.major = major;
         this.minor = minor;
+    }
+
+    public static Browser getDefaultBrowser() {
+        return DEFAULT_BROWSER;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public String getMinor() {
+        return minor;
     }
 }
--- src/main/java/com/tiger/useragent/Device.java
+++ src/main/java/com/tiger/useragent/Device.java
@@ -8,16 +8,16 @@ import static com.tiger.useragent.Constant.DEFAULT_VALUE;
  * email : dayingzhaolihe@126.com
  * date : 2017/5/9
  */
-class Device {
-    public static final Device DEFAULT_PC_SCREEN = new Device("PC", DEFAULT_VALUE, DeviceType.PC, false, DEFAULT_VALUE);
-    public static final Device DEFAULT_PHONE_SCREEN = new Device(DEFAULT_VALUE, DEFAULT_VALUE, DeviceType.Phone, true, DEFAULT_VALUE);
-    public static final Device DEFAULT_TV = new Device(DEFAULT_VALUE, DEFAULT_VALUE, DeviceType.TV, false, DEFAULT_VALUE);
+public class Device {
+    static final Device DEFAULT_PC_SCREEN = new Device("PC", DEFAULT_VALUE, DeviceType.PC, false, DEFAULT_VALUE);
+    static final Device DEFAULT_PHONE_SCREEN = new Device(DEFAULT_VALUE, DEFAULT_VALUE, DeviceType.Phone, true, DEFAULT_VALUE);
+    static final Device DEFAULT_TV = new Device(DEFAULT_VALUE, DEFAULT_VALUE, DeviceType.TV, false, DEFAULT_VALUE);
 
-    public final String brand;
-    public final String family;
-    public final DeviceType deviceType;
-    public final boolean isMobile;
-    public final String screenSize;
+    final String brand;
+    final String family;
+    final DeviceType deviceType;
+    final boolean isMobile;
+    final String screenSize;
 
     Device(String brand, String family, DeviceType deviceType, boolean isMobile) {
         this.brand = brand;
@@ -33,5 +33,25 @@ class Device {
         this.deviceType = deviceType;
         this.isMobile = isMobile;
         this.screenSize = screenSize;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public DeviceType getDeviceType() {
+        return deviceType;
+    }
+
+    public boolean isMobile() {
+        return isMobile;
+    }
+
+    public String getScreenSize() {
+        return screenSize;
     }
 }
